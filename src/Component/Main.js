3540fc8a65986b3adc53b015662c0f4ab53f7faa
ooyake_chan <tendouzi1105@gmail.com>
2020-09-09 14:40:40 +0900

    黒背景とスタンプ欄のサイズの修正

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -132,10 +132,10 @@ class Main extends Component{
           <h2>{date}</h2>
         </div>
         <Stamps />
-      </div>
         <Setting colorChange={colorChange} stampChange={stampChange} fontChange={fontChange}/>
         <Modal id="img_up_modal" name="画像のアップロード" style={img_modal_Style} body={img_modal_Body}/>
         <Modal id="reset_modal" name="リセット" style={reset_modal_Style} body={reset_modal_Body}/>
+      </div>
     </main>
   }
 }
