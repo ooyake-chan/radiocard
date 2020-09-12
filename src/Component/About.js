86512fb0ce84f116c5b5cb78ba301a6f276d1398
ooyake_chan <tendouzi1105@gmail.com>
2020-09-12 11:17:31 +0900

    シェアボタンを削除

--- src/Component/About.js
+++ src/Component/About.js
@@ -21,12 +21,6 @@ export function About(props){
         <h2>使用素材</h2>
         <a href="https://www.photo-ac.com/?gclid=CjwKCAjwqML6BRAHEiwAdquMnQZGGmN4ye2uqxERH7r1FFrfHV9k2xHbGVZsm55Ap-BTdoIDw-vZdRoCPZ0QAvD_BwE" target="_blank">写真AC</a><br/>
         <a href="https://icooon-mono.com/" target="_blank">ICOON MONO</a><br/>
-        <hr/>
-        <h2>シェア</h2>
-        <a target="blank" href="https://twitter.com/intent/tweet?url=[http://ooyakecyan.html.xdomain.jp/cardapp/index.html]&text=[スタンプカードapp]"><Twitter /></a>
-        <a target="blank" href="https://www.facebook.com/sharer/sharer.php?u=[http://ooyakecyan.html.xdomain.jp/cardapp/index.html]"><Facebook /></a>
-        <a target="blank" href="http://line.me/R/msg/text/?[スタンプカードapp]/n[http://ooyakecyan.html.xdomain.jp/cardapp/index.html]"><Line /></a>
-
         <div className="center">
           <a className="blue_btn" onClick={()=> props.modeAction('HOW')}>前の画面にもどる</a>
         </div>
